dbaa27ba5f4ec6511ba7e52913c2a76d3c01b972
GuinhoNote <macharetevic@gmail.com>
2023-04-13 00:59:51 -0300

    Área do quadrado incompleta;

--- app/src/main/java/com/example/areas/MainActivity.java
+++ app/src/main/java/com/example/areas/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.areas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnQuadrado;
+
+    Intent in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Cal cal = new Cal();
+        btnQuadrado = findViewById(R.id.btnQuadrado);
+
+        btnQuadrado.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            in = new Intent(MainActivity.this, activityQuadrado.class);
+            startActivity(in);
+            }
+        });
+
+
 
 
 
--- app/src/main/java/com/example/areas/activityQuadrado.java
+++ app/src/main/java/com/example/areas/activityQuadrado.java
@@ -2,13 +2,40 @@ package com.example.areas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activityQuadrado extends AppCompatActivity {
 
+    Button btnCalcQdrd;
+    EditText edValorQdrd;
+    TextView tvResultQdrd;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quadrado);
+        btnCalcQdrd = findViewById(R.id.btnCalcQdrd);
+        edValorQdrd = findViewById(R.id.edValorQdrd);
+        tvResultQdrd = findViewById(R.id.tvResultQdrd);
+        btnCalcQdrd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cal cal = new Cal();
+                String valStr = edValorQdrd.getText().toString();
+                float valF = Float.parseFloat(valStr);
+                valStr = String.valueOf(cal.CalQuadrado(valF));
+                tvResultQdrd.setText(valStr);
+
+
+
+
+
+            }
+        });
     }
 }
