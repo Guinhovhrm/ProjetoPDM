eaa260377a7a555d2096cf2cb89336104f9b39e4
guinho <macharetevic@gmail.com>
2023-04-19 16:42:07 -0300

    Activities criadas

--- app/src/main/java/com/example/areas/MainActivity.java
+++ app/src/main/java/com/example/areas/MainActivity.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnQuadrado;
+    Button btnQuadrado, btnTriangulo, btnTriEqui, btnHexagono, btnRetangulo, btnTrapezio, btnCirc;
 
     Intent in;
     @Override
@@ -17,16 +17,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnQuadrado = findViewById(R.id.btnQuadrado);
+        btnTriangulo = findViewById(R.id.btnTriangulo);
+        btnTriEqui = findViewById(R.id.btnTriEqui);
+        btnHexagono = findViewById(R.id.btnHexagono);
+        btnRetangulo = findViewById(R.id.btnRetangulo);
+        btnTrapezio = findViewById(R.id.btnTrapezio);
+        btnCirc = findViewById(R.id.btnCirc);
 
         btnQuadrado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            in = new Intent(MainActivity.this, activityQuadrado.class);
+                in = new Intent(MainActivity.this, activityQuadrado.class);
             startActivity(in);
             }
         });
+        btnTriangulo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                in = new Intent(MainActivity.this, activityTriangulo.class);
+                startActivity(in);
+            }
+        });
+        btnTriEqui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                in = new Intent(MainActivity.this, activityTriEqui.class);
+                startActivity(in);
 
+            }
+        });
+        btnHexagono.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+        btnRetangulo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnTrapezio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        btnCirc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
 
 
 
---
+++ app/src/main/java/com/example/areas/activityTriEqui.java
@@ -0,0 +1,14 @@
+package com.example.areas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activityTriEqui extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tri_equi);
+    }
+}
