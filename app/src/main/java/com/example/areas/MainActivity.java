1076c44b67e8c5f4a56a0a7a7120abf12cc4bc0d
Guinhovhrm <macharetevic@gmail.com>
2023-04-20 02:07:58 -0300

    Hexagono foi; Triangulo e triangulo equilatero foi; quadrado foi.

--- app/src/main/java/com/example/areas/MainActivity.java
+++ app/src/main/java/com/example/areas/MainActivity.java
@@ -9,9 +9,9 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnQuadrado, btnTriangulo, btnTriEqui, btnHexagono, btnRetangulo, btnTrapezio, btnCirc;
-
+    Button btnQuadrado, btnTriangulo, btnTriEqui, btnHex, btnRetangulo, btnTrapezio, btnCirc;
     Intent in;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         btnQuadrado = findViewById(R.id.btnQuadrado);
         btnTriangulo = findViewById(R.id.btnTriangulo);
         btnTriEqui = findViewById(R.id.btnTriEqui);
-        btnHexagono = findViewById(R.id.btnHexagono);
+        btnHex = findViewById(R.id.btnHex);
         btnRetangulo = findViewById(R.id.btnRetangulo);
         btnTrapezio = findViewById(R.id.btnTrapezio);
         btnCirc = findViewById(R.id.btnCirc);
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 in = new Intent(MainActivity.this, activityQuadrado.class);
-            startActivity(in);
+                startActivity(in);
             }
         });
         btnTriangulo.setOnClickListener(new View.OnClickListener() {
@@ -46,27 +46,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        btnHexagono.setOnClickListener(new View.OnClickListener() {
+        btnHex.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                in = new Intent(MainActivity.this, activityHexagono.class);
+                startActivity(in);
             }
         });
         btnRetangulo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                in = new Intent(MainActivity.this, activityRetangulo.class);
+                startActivity(in);
 
             }
         });
         btnTrapezio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                in = new Intent(MainActivity.this, activityTrapezio.class);
+                startActivity(in);
 
             }
         });
         btnCirc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                in = new Intent(MainActivity.this, activityCirculo.class);
+                startActivity(in);
 
             }
         });
--- app/src/main/java/com/example/areas/activityHexagono.java
+++ app/src/main/java/com/example/areas/activityHexagono.java
@@ -3,12 +3,29 @@ package com.example.areas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activityHexagono extends AppCompatActivity {
 
+    Button btnCalcHex;
+    EditText edLadoHex;
+    TextView tvResultHex;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hexagono);
+        btnCalcHex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cal cal = new Cal();
+                String valStr = edLadoHex.getText().toString();
+                float valF = Float.parseFloat(valStr);
+                valStr = String.valueOf(cal.CalHexagono(valF));
+                tvResultHex.setText(valStr);
+            }
+        });
     }
 }
