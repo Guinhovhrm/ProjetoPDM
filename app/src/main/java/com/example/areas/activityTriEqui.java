047716988c27df07693d928c3cf7e17fe61b6064
Guinhovhrm <macharetevic@gmail.com>
2023-04-19 21:04:20 -0300

    Inicio hexagono

--- app/src/main/java/com/example/areas/activityTriEqui.java
+++ app/src/main/java/com/example/areas/activityTriEqui.java
@@ -3,9 +3,15 @@ package com.example.areas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activityTriEqui extends AppCompatActivity {
 
+    Button btnCalcTriEqui;
+    EditText edLadoTriEqui;
+    TextView tvResultTriEqui;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
