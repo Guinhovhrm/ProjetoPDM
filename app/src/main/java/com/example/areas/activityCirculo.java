00ce555c3722a22bd5ad0606bc440e55f377e4e2
Guinhovhrm <macharetevic@gmail.com>
2023-04-20 02:19:57 -0300

    Circulo completo; arrumado "altura" no triangulo.

--- app/src/main/java/com/example/areas/activityCirculo.java
+++ app/src/main/java/com/example/areas/activityCirculo.java
@@ -2,13 +2,39 @@ package com.example.areas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class activityCirculo extends AppCompatActivity {
 
+    Button btnCalcCirc;
+    EditText edRaioCirc;
+    TextView tvResultCirc;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_circulo);
+        btnCalcCirc = findViewById(R.id.btnCalcCirc);
+        edRaioCirc = findViewById(R.id.edRaioCirc);
+        tvResultCirc = findViewById(R.id.tvResultCirc);
+
+        btnCalcCirc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cal cal = new Cal();
+                String valStr = edRaioCirc.getText().toString();
+                float valF = Float.parseFloat(valStr);
+                valStr = String.valueOf(cal.CalCirculo(valF));
+                tvResultCirc.setText(valStr);
+
+            }
+        });
+
     }
 }
