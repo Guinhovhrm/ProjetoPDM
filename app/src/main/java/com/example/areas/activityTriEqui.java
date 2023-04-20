1b0e05a860bf68d2ff8994b04350520ac064d05b
Guinhovhrm <macharetevic@gmail.com>
2023-04-19 23:30:39 -0300

    Inicio hexagono

--- app/src/main/java/com/example/areas/activityTriEqui.java
+++ app/src/main/java/com/example/areas/activityTriEqui.java
@@ -3,12 +3,15 @@ package com.example.areas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class activityTriEqui extends AppCompatActivity {
+import java.text.DecimalFormat;
 
+public class activityTriEqui extends AppCompatActivity {
+    DecimalFormat df = new DecimalFormat("0.00");
     Button btnCalcTriEqui;
     EditText edLadoTriEqui;
     TextView tvResultTriEqui;
@@ -16,5 +19,20 @@ public class activityTriEqui extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tri_equi);
+        btnCalcTriEqui = findViewById(R.id.btnCalcTriEqui);
+        edLadoTriEqui = findViewById(R.id.edLadoTriEqui);
+        tvResultTriEqui = findViewById(R.id.tvResultTriEqui);
+
+        btnCalcTriEqui.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Cal cal = new Cal();
+                String valStr = edLadoTriEqui.getText().toString();
+                float valF = Float.parseFloat(valStr);
+                valStr = String.valueOf(cal.CalTrianguloEquil(valF));
+                tvResultTriEqui.setText(valStr);
+            }
+        });
+
     }
 }
